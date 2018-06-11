0faf6f0e3c4f7da5a85fe17a18e387cfce1bfcf2
Haskel Ash <haskelash@gmail.com>
2018-06-10 19:38:48 -0700

    add Header component and use it in index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/header';
+
+const App = () => (
+    <Header />
+);
 
 AppRegistry.registerComponent('albums', () => App);
