60f8da348b47abeb04928eedb35b6d34dfe0fc66
Haskel Ash <haskelash@gmail.com>
2018-06-15 16:36:11 -0700

    get rid of mention of index.js in import

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header, Button, Spinner } from './components/common/index.js';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
