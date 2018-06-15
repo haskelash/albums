2d486d18a9fb77ad76fc08f5529f1d597cb8db35
Haskel Ash <haskelash@gmail.com>
2018-06-15 10:14:01 -0700

    add firebase to dependencies, config firebase in App.js

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common/index.js';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDydq_Ev_OPLEJLgAnlNNPkjOT_0JqhVbg',
+      authDomain: 'auth-f681d.firebaseapp.com',
+      databaseURL: 'https://auth-f681d.firebaseio.com',
+      projectId: 'auth-f681d',
+      storageBucket: 'auth-f681d.appspot.com',
+      messagingSenderId: '303682792301'
+    });
+  }
+
   render() {
     return (
       <View>
