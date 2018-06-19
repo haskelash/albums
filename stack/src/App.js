e000fea8b94694baee30e1e7ea9de066fefe905b
Haskel Ash <haskelash@gmail.com>
2018-06-18 17:19:51 -0700

    add LibraryList component, feed it static json

--- stack/src/App.js
+++ stack/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => (
-  <Provider store={createStore(reducers)}>
-    <Header headerText="Stack" />
-  </Provider>
-);
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Stack" />
+        {<LibraryList />}
+      </View>
+    </Provider>
+  );
 
 export default App;
