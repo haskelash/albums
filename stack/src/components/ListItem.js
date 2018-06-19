7ee33970d09b689ac7731dd471a76efe7f675acd
Haskel Ash <haskelash@gmail.com>
2018-06-19 12:21:00 -0700

    move expanded logic to mapStateToProps

--- stack/src/components/ListItem.js
+++ stack/src/components/ListItem.js
@@ -6,8 +6,8 @@ import * as actions from '../actions';
 
 class ListItem extends Component {
   renderDescription() {
-    const { library, selectedLibraryId } = this.props;
-    if (library.id === selectedLibraryId) {
+    const { library, expanded } = this.props;
+    if (expanded) {
       return (
         <Text>{library.description}</Text>
       );
@@ -41,6 +41,9 @@ const styles = {
   }
 };
 
-const mapStateToProps = state => ({ selectedLibraryId: state.selectedLibraryId });
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded };
+};
 
 export default connect(mapStateToProps, actions)(ListItem);
