b03b95ab694ff99ca4d0da35489ebd6e89c0aa01
Haskel Ash <haskelash@gmail.com>
2018-06-20 13:39:11 -0700

    add firebase, add config to componentWillMount

--- navigation/src/App.js
+++ navigation/src/App.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyDQ1BA7SGLxeVU-zBh0oSQNSzhZ1uwlUCU',
+      authDomain: 'navigation-9ce74.firebaseapp.com',
+      databaseURL: 'https://navigation-9ce74.firebaseio.com',
+      projectId: 'navigation-9ce74',
+      storageBucket: 'navigation-9ce74.appspot.com',
+      messagingSenderId: '318327145387'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
