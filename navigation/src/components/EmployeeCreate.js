c90c267cfd45bcc89d27ae5ea1342aa2e1e5a611
Haskel Ash <haskelash@gmail.com>
2018-06-27 11:40:32 -0700

    fill out the employee create form

--- navigation/src/components/EmployeeCreate.js
+++ navigation/src/components/EmployeeCreate.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, CardItem, Field, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Bar</Text>
-      </View>
+      <Card>
+        <CardItem>
+          <Field
+            label="Name"
+            placeholder="John Doe"
+          />
+        </CardItem>
+        <CardItem>
+          <Field
+            label="Phone"
+            placeholder="555-555-5555"
+          />
+        </CardItem>
+
+        <CardItem />
+
+        <CardItem>
+          <Button>
+            Create
+          </Button>
+        </CardItem>
+      </Card>
     );
   }
 }
