44d31041cd602eb6a8fe8a12654de4f2e6b8c832
Haskel Ash <haskelash@gmail.com>
2018-06-27 10:43:22 -0700

    add EmployeeList component, add it to router, make login scene initial

--- navigation/src/Router.js
+++ navigation/src/Router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => (
   <Router>
     <Scene key="root">
-      <Scene key="login" component={LoginForm} title="Login / Sign Up" />
+      <Scene key="login" component={LoginForm} title="Login / Sign Up" initial />
+      <Scene key="employeeList" component={EmployeeList} title="Employees" />
     </Scene>
   </Router>
 );
