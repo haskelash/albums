f1edf7e15b0a868598754f19b32752d2bcc58485
Haskel Ash <haskelash@gmail.com>
2018-06-28 16:25:18 -0700

    add state to props function, use lodash to convert from object to array, feed into listview

--- navigation/src/components/EmployeeList.js
+++ navigation/src/components/EmployeeList.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { fetchEmployees } from '../actions';
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.fetchEmployees();
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource(props) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(props.employees);
   }
 
   render() {
@@ -17,4 +30,9 @@ class EmployeeList extends Component {
   }
 }
 
-export default connect(null, { fetchEmployees })(EmployeeList);
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => ({ ...val, uid }));
+  return { employees };
+};
+
+export default connect(mapStateToProps, { fetchEmployees })(EmployeeList);
