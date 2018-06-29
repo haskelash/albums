8d588ad09e83003927a98e60f667ce41572952f2
Haskel Ash <haskelash@gmail.com>
2018-06-29 10:25:16 -0700

    have list item go to edit form instead of create form, prefill form with employee data, log to console on button press

--- navigation/src/components/EmployeeEdit.js
+++ navigation/src/components/EmployeeEdit.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { Card, CardItem, Button } from './common';
+import { updateEmployeeForm } from '../actions';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
+  componentWillMount() {
+    _.each(this.props.employee, (value, prop) => {
+      this.props.updateEmployeeForm({ prop, value });
+    });
+  }
+
+  onButtonPress() {
+    console.log(this.props);
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
         <CardItem>
-          <Button>Save Changes</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
+            Save Changes
+          </Button>
         </CardItem>
       </Card>
     );
   }
 }
 
-export default connect()(EmployeeEdit);
+const mapStateToProps = ({ employeeForm }) => {
+  const { name, phone, shift } = employeeForm;
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { updateEmployeeForm })(EmployeeEdit);
